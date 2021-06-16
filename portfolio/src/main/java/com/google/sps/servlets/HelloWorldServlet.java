10d94a02e878ff555c4814917fc5670c8e490e41
Miguel <EcheverriaSoftware@gmail.com>
2021-06-16 21:11:08 +0000

    Implemented fetch in script.js to retireve a random quote from HelloWorldServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,13 +6,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-  @Override
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String [] movieQuotes = {"Get to the choppa ", "Hasta la vista baby ","I will be back ", "You have been terminated."};
+
+    @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+      
+    String json = convertToJsonUsingGson(movieQuotes);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+  private String convertToJsonUsingGson(String [] quotes) {
+    Gson gson = new Gson();
+    String json = gson.toJson(quotes);
+    return json;
   }
 }
